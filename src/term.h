ec15c20e91e1f233c629adee9a219d723f50d710
rylenko <rylenko@tuta.io>
2024-03-02 04:30:28 +0700

    settings -> parameters. Some corrections.

--- src/term.h
+++ src/term.h
@@ -4,15 +4,10 @@
 #include <termios.h>
 #include "res.h"
 
-/*
-Disables raw mode. Accepts original settings.
-*/
+/* Disables raw mode. Accepts original parameters. */
 Res term_disable_raw_mode(const struct termios*);
 
-/*
-Enables raw mode. Accepts a pointer where the original settings will be
-written.
-*/
+/* Enables raw mode. Accepts a pointer to save the original parameters. */
 Res term_enable_raw_mode(struct termios*);
 
 #endif /* _TERM_H */
