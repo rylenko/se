fbf7105bb7c2d928b68c19fd1c72b4076eae6803
rylenko <rylenko@tuta.io>
2024-03-15 13:26:59 +0700

    Arrows movement

--- src/ed.c
+++ src/ed.c
@@ -1,3 +1,4 @@
+/* TODO: Arrows */
 /* TODO: Add local clipboard. Use it in functions. */
 /* TODO: Use linked list for rows array and row's content parts */
 /* TODO: Integrate repetition of keys into handlers */
