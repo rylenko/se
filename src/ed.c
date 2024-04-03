adacdfebbe1362ef75285e166f542ba68ed0f44b
rylenko <rylenko@tuta.io>
2024-04-04 06:28:23 +0700

    Replace void with static void where needed

--- src/ed.c
+++ src/ed.c
@@ -348,61 +348,61 @@ ed_ins_empty_line_on_top(Ed *const ed)
 	ed->mode = MODE_INS;
 }
 
-void
+static void
 ed_mv_down(Ed *const ed)
 {
 	win_mv_down(ed->win, ed_repeat_times(ed));
 }
 
-void
+static void
 ed_mv_left(Ed *const ed)
 {
 	win_mv_left(ed->win, ed_repeat_times(ed));
 }
 
-void
+static void
 ed_mv_right(Ed *const ed)
 {
 	win_mv_right(ed->win, ed_repeat_times(ed));
 }
 
-void
+static void
 ed_mv_up(Ed *const ed)
 {
 	win_mv_up(ed->win, ed_repeat_times(ed));
 }
 
-void
+static void
 ed_mv_to_begin_of_file(Ed *const ed)
 {
 	win_mv_to_begin_of_file(ed->win);
 }
 
-void
+static void
 ed_mv_to_begin_of_line(Ed *const ed)
 {
 	win_mv_to_begin_of_line(ed->win);
 }
 
-void
+static void
 ed_mv_to_end_of_file(Ed *const ed)
 {
 	win_mv_to_end_of_file(ed->win);
 }
 
-void
+static void
 ed_mv_to_end_of_line(Ed *const ed)
 {
 	win_mv_to_end_of_line(ed->win);
 }
 
-void
+static void
 ed_mv_to_next_word(Ed *const ed)
 {
 	win_mv_to_next_word(ed->win, ed_repeat_times(ed));
 }
 
-void
+static void
 ed_mv_to_prev_word(Ed *const ed)
 {
 	win_mv_to_prev_word(ed->win, ed_repeat_times(ed));
