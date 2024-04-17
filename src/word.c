5738c81e5593bc25e46bc6f4f395500fe64dfafd
rylenko <rylenko@tuta.io>
2024-04-18 03:32:30 +0700

    Fix word_rnext

--- src/word.c
+++ src/word.c
@@ -20,25 +20,23 @@ word_next(const char *const str, const size_t len)
 }
 
 size_t
-word_rnext(const char *const str, const size_t len)
+word_rnext(const char *const str, const size_t pos)
 {
 	size_t i;
 	char space_visited = 0;
 	char prev_word_visited = 0;
 
-	if (len > 1) {
-		for (i = len - 1; i > 0; i--) {
-			/* First appearance of non space after spaces */
-			if (!isspace(str[i]) && space_visited)
-				prev_word_visited = 1;
-			/* First appearance of space */
-			else if (isspace(str[i]) && !space_visited)
-				space_visited = 1;
-			/* Second appearance of spaces after word */
-			else if (isspace(str[i]) && space_visited && prev_word_visited)
-				/* Start of word before second spaces */
-				return i + 1;
-		}
+	for (i = pos; i > 0; i--) {
+		/* First appearance of non space after spaces */
+		if (!isspace(str[i]) && space_visited)
+			prev_word_visited = 1;
+		/* First appearance of space */
+		else if (isspace(str[i]) && !space_visited)
+			space_visited = 1;
+		/* Second appearance of spaces after word */
+		else if (isspace(str[i]) && prev_word_visited)
+			/* Start of word before second spaces */
+			return i + 1;
 	}
 	return 0;
 }
